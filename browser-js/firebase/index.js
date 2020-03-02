982c7a923dd8f7027dc4d18137cd8543c41d6591
Simon Hoyos <shmesa23@gmail.com>
2020-03-02 09:27:33 -0500

    Add mar 2 morning class

--- browser-js/firebase/index.js
+++ browser-js/firebase/index.js
@@ -1,18 +1,22 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyDXQXvwcJr2S1ZqNcd-VxQ17g-eW61p0kM",
+  authDomain: "dummy-fcd18.firebaseapp.com",
+  databaseURL: "https://dummy-fcd18.firebaseio.com",
+  projectId: "dummy-fcd18",
+  storageBucket: "dummy-fcd18.appspot.com",
+  messagingSenderId: "938319458028",
+  appId: "1:938319458028:web:0fcb507d3aaf16c9e7bfc9"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.firestore();
 const usersCollection = database.collection('users');
+const postsCollection = database.collection('posts');
+
+// 1:1, 1:N, N:N
+const posts = document.querySelector('.posts');
 
 // CRUD
 // Create Read Update Delete
@@ -65,9 +69,40 @@ const data = {
 //   .then(() => console.log('Todo OK!'))
 //   .catch(() => console.log('Algo salió mal'));
 
-const commentsCollection = database.collection('comments');
+// const commentsCollection = database.collection('comments');
 
-commentsCollection.add({
-  title: 'comment',
-  body: 'Un comentario muy largoooooooooooo!'
-})
+// commentsCollection.add({
+//   title: 'comment',
+//   body: 'Un comentario muy largoooooooooooo!'
+// })
+
+// 1:1
+// postsCollection.get()
+//   .then(docs => {
+//     docs.forEach(doc => {
+//       const postData = doc.data();
+
+//       console.log(postData);
+//       usersCollection.doc(postData.author).get()
+//         .then(doc => {
+//           const userData = doc.data();
+//           const postHtml = `
+//             <div class="post">
+//               <h2>${postData.title}</h2>
+//               <p>${postData.body}</p>
+//               <span>By: ${userData.name}</span>
+//             </div>
+//           `;
+
+//           posts.innerHTML += postHtml;
+//         });
+//     });
+//   })
+
+// 1:N
+usersCollection.doc('YfqUHkRwrQvK3h8vNXuF').get()
+  .then(doc => {
+    const userData = doc.data();
+
+    console.log(userData);
+  })
