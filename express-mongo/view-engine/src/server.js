583297c0a9ca1be27a6534c5c9708a3756e16705
Simon Hoyos <shmesa23@gmail.com>
2020-03-04 15:23:27 -0500

    Add mar 4 afternoon class

--- express-mongo/view-engine/src/server.js
+++ express-mongo/view-engine/src/server.js
@@ -1,12 +1,43 @@
 const express = require('express');
+const uuid = require('uuid-random');
 
 const app = express();
 
 app.set('view engine', 'pug');
 // app.set('views', './views'); // opcional
 
-app.get('/messages', (req, res) => {
-  res.render('index');
+const messages = [
+  {
+    sender: 'Simon',
+    message: 'Hola',
+    id: uuid(),
+  },
+  {
+    sender: 'Maria',
+    message: 'Mundo',
+    id: uuid(),
+  }
+];
+
+app.get('/', (req, res) => {
+  res.render(
+    'index',
+    {
+      title: 'Un titulo',
+      description: 'lorem ipsum dolor sit ammet',
+      authenticated: true
+    }
+  );
 });
+
+app.get('/messages', (req, res) => {
+  res.render('messages', { messages });
+});
+
+app.get('/messages/:id', (req, res) => {
+  const id = req.params.id;
+  const message = messages.filter(message => message.id === id)[0];
+  res.render('message', message);
+})
 
 app.listen(3000, () => console.log(`Server listening on http://localhost:3000`));
